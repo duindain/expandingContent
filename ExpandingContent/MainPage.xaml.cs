ee71f46f31bb3f1702e1068c68308b43a3aa02fa
Richard Humphrey <richard.humphrey@health.gov.au>
2022-10-20 12:55:04 +1100

    Initial commit

---
+++ ExpandingContent/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace ExpandingContent;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		//Work around for button missing for some reason when the boxview is enlarged
+		var vm = BindingContext as MainPageVM;
+		vm?.ChangeHeightOfBoxViewCmd?.Execute(null);
+
+    }
+}
+
+
---
+++ ExpandingContent/MainPageVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ExpandingContent
+{
+    public class MainPageVM : BindableBase
+    {
+        public ICommand ChangeHeightOfBoxViewCmd { get; set; }
+
+        private double _boxHeight = 200d;
+        public double BoxHeight
+        {
+            get => _boxHeight;
+            set => SetProperty(ref _boxHeight, value);
+        }
+
+        public MainPageVM()
+        {
+            ChangeHeightOfBoxViewCmd = new Command(() =>
+            {
+                BoxHeight = BoxHeight >= 200d ? 30d : 200d;
+                System.Diagnostics.Debug.WriteLine($"Height of box changed to {BoxHeight}");
+            });
+        }
+    }
+}
+
